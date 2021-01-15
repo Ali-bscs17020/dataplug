3757aa1ae82a04203bcec33bcde6ec7b7d0cc759
Ali-bscs17020 <77493872+Ali-bscs17020@users.noreply.github.com>
2021-01-15 18:28:47 +0500

    Update complaint.php

--- application/controllers/complaint.php
+++ application/controllers/complaint.php
@@ -370,8 +370,8 @@ class Complaint extends CI_Controller {
                                 $this->data['error'] = $this->upload->display_errors();
                                 $this->session->set_flashdata('validate', array('message' => $this->upload->display_errors(), 'type' => 'warning'));
                             } else {
-                                $imageData = $this->upload->data();
-                                $fileName = $imageData['file_name'];                                
+                                $image_data = $this->upload->data();
+                                $file_name = $imageData['file_name'];                                
                                 $image_array=array('cp_photo'=>$fileName);
                             }
                         }
