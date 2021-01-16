5015f58402e3c13498aa7dac71d5b08def988902
Ali-bscs17020 <77493872+Ali-bscs17020@users.noreply.github.com>
2021-01-16 17:19:34 +0500

    Update app_installed_model.php
    correction of naming format

--- application/models/app_installed_model.php
+++ application/models/app_installed_model.php
@@ -13,7 +13,7 @@ class App_Installed_model extends CI_Model {
      * @return type
      * @author Zahid Nadeem <zahidiubb@yahoo.com>
      */
-    public function get_app_installed($app_id,$imei_no) {
+    public function getAppInstalled($app_id,$imei_no) {
 
         $query = $this->db->get_where('app_installed', array('app_id' => $app_id,'imei_no' => $imei_no));
        
@@ -27,7 +27,7 @@ class App_Installed_model extends CI_Model {
      * @return type
      * @author Zahid Nadeem <zahidiubb@yahoo.com>
      */
-    public function get_app_installed_byappid($app_id) {
+    public function getAppInstalledByAppId($app_id) {
         
         $query = $this->db->get_where('app_installed', array('app_id' => $app_id));
         return $query->result_array();
