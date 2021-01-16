2ce02c4a03be69868c732795edf0a7f641171c76
Ali-bscs17020 <77493872+Ali-bscs17020@users.noreply.github.com>
2021-01-16 17:51:18 +0500

    Update common.php

--- application/controllers/common.php
+++ application/controllers/common.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
-class Common extends CI_Controller {
+class Common_Extends_CI_Controller {
 
     public function __construct() {
         parent::__construct();
