ddde46551f4ba5e5e526eb2a655d1209213cf9e5
Ali-bscs17020 <77493872+Ali-bscs17020@users.noreply.github.com>
2021-01-16 17:42:08 +0500

    Update settings_model.php

--- application/models/settings_model.php
+++ application/models/settings_model.php
@@ -12,7 +12,7 @@ class Settings_model extends CI_Model {
      * @return value
      * @author Zahid Nadeem <zahidiubb@yahoo.com>
      */
-    public function get_param($slug = FALSE) {
+    public function getParam($slug = FALSE) {
          $query = $this->db->get_where('settings', array('key' => $slug));
          $rec = $query->row_array();
 
